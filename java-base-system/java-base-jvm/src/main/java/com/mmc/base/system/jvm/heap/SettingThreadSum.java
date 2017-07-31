35fc6a11cd2a05ea2ab80084ec5aa19f6886f62b
gaowei0115 <gaowei@gaowei-PC>
2017-07-31 17:55:42 +0800

    thread sum jvm

--- java-base-system/java-base-jvm/src/main/java/com/mmc/base/system/jvm/heap/SettingThreadSum.java
+++ java-base-system/java-base-jvm/src/main/java/com/mmc/base/system/jvm/heap/SettingThreadSum.java
@@ -23,10 +23,19 @@ public class SettingThreadSum {
 			for (i = 0; i < 10000; i++) {
 				new ThreadSum().start();
 			}
-		} catch (Exception e) {
+		} catch (Throwable e) {
 			System.out.println("count thread sum : " + i);
+			e.printStackTrace();
 		}
 	}
+	
+	/**
+	 * java.lang.OutOfMemoryError: unable to create new native thread
+	at java.lang.Thread.start0(Native Method)
+	at java.lang.Thread.start(Thread.java:714)
+	at com.mmc.base.system.jvm.heap.SettingThreadSum.main(SettingThreadSum.java:24)
+count thread sum : 639
+	 */
 	
 	/**
 	 * className: ThreadSum<br/>
